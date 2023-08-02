a8a130e835086dc937e0e02a9db0c46a8480bf10
FarjanaAkterLaila <farjanakter7275@gmail.com>
2023-08-02 20:28:31 +0600

    show data

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zmwk4eu.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+const classesCollection = client.db("internbd").collection("class");
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+     //classes
+     app.get('/classes', async (req, res) => {
+      const query = {};
+     
+      const cursor = classesCollection.find( query);
+      const result = await cursor.toArray();
+  
+      res.send(result);
+    })
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send('running....')
+})
+
+app.listen(port,()=>{
+    console.log(`port running ${port}`);
+})
